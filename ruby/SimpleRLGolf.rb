012eb36562fbd310e38b3cdf80526d6145020951
Paul Prestidge <chronicstar@gmail.com>
2011-03-10 20:11:50 -0800

    Version using only one variable for position ($p). 277.

--- ruby/SimpleRLGolf.rb
+++ ruby/SimpleRLGolf.rb
@@ -1,9 +1,8 @@
 require 'curses';include Curses
-def d t=?@;setpos $y||=2,$x||=2;addch t;end
+def d t=?@;setpos $p/10,$p%10;addch t;end
 s=init_screen;noecho;curs_set 0;s.keypad 1
-b=0xf3e798070340902c0e019e7cf.to_s(2).tr('01',' #').scan /.{10}/
-addstr b*$/;d
-while v='5317'[s.getch-258]-?0 rescue exit
-x,y=$x+v/3-1,$y+v%3-1
-d 32;$x,$y=x,y if b[y][x]==32;d;
+b=0xf3e798070340902c0e019e7cf.to_s(2).tr('01',' #')
+addstr b.scan(/.{10}/)*$/;$p=22;d
+while v='aMVX'[s.getch-258]-?W rescue exit
+d 32;$p+=v if b[$p+v]==32;d
 end
