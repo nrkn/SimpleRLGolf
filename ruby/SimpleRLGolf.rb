3e31c7173f5dacd24bbeb32d90a3e626a988ca41
Paul Prestidge <chronicstar@gmail.com>
2012-03-01 17:21:09 +1300

    Update ruby/SimpleRLGolf.rb

--- ruby/SimpleRLGolf.rb
+++ ruby/SimpleRLGolf.rb
@@ -4,5 +4,5 @@ s=init_screen;noecho;curs_set 0;s.keypad 1
 b=0xf3e798070340902c0e019e7cf.to_s(2).tr'01',' #'
 addstr b.scan(/.{10}/)*$/;$p=22;d
 while v=$p+[10,-10,-1,1][s.getch-258]
-d 32;$p=v if b[v]==' ';d
+d 32;$p=v if b[v]<?#;d
 end
